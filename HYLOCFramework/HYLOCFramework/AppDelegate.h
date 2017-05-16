af64e3ae51feae40689483e3df2ee665454084eb
戴超 <965524749@qq.com>
2017-05-16 11:09:06 +0800

    feat：init project

---
+++ HYLOCFramework/HYLOCFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYLOCFramework
+//
+//  Created by JF on 2017/5/16.
+//  Copyright © 2017年 HYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYLOCFramework/HYLOCFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYLOCFramework
+//
+//  Created by JF on 2017/5/16.
+//  Copyright © 2017年 HYL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
